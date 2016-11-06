e6eb10d68666769ac106851b15a1b12ff4be7d6b
Christopher Pollati <christopher_pollati@bloomfield.edu>
2016-11-05 20:06:14 -0400

    Initial commit of project files.
    Modified .gitignore to exclude folders from all directories, not just the root folder. Also, updated readme.md and added images to it.

---
+++ ProfPollatiUtilities/Assets/Scripts/Editor/BuildBuddy.cs
@@ -0,0 +1,58 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using System.Diagnostics;
+using UnityEngine;
+using System.IO.Compression;
+using System.IO;
+using Ionic.Zip;
+
+public class BuildBuddy : MonoBehaviour {
+	[MenuItem("Pollati Utilities/Build Buddy")]
+	public static void BuildGame () {
+		BuildTarget[] targets = {
+			BuildTarget.StandaloneLinuxUniversal,
+			BuildTarget.StandaloneOSXIntel64,
+			BuildTarget.StandaloneWindows,
+			BuildTarget.StandaloneWindows64
+		};
+
+		string[] targetPrefix = {"-Linux","-Mac","-Win_x86","-Win_x86-64"};
+
+		string baseDir = "Builds" + Path.DirectorySeparatorChar;
+		string projectName = PlayerSettings.productName;
+		string buildPath;
+		string buildName;
+
+		for(var i=0;i<targets.Length;i++) {
+			// Figure the name of the folder to build to
+			buildPath = baseDir + projectName + targetPrefix [i];
+
+			// Figure the extension for the executable
+			if (targets[i]==BuildTarget.StandaloneOSXIntel) {
+				buildName = projectName + ".app";
+			} else {
+				buildName = projectName + ".exe";
+			}
+
+			// Build
+			UnityEngine.Debug.Log ("Building"+targetPrefix[i]);
+			BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, buildPath + Path.DirectorySeparatorChar + buildName, targets[i], BuildOptions.None);
+
+			// Zip
+			UnityEngine.Debug.Log ("Zipping"+targetPrefix[i]);
+			using (ZipFile zip = new ZipFile()) {
+				zip.AddDirectory (buildPath + Path.DirectorySeparatorChar);
+				zip.Save(buildPath + ".zip");
+			}
+
+			// Done
+			UnityEngine.Debug.Log ("Building"+targetPrefix[i]+ " Complete!");
+		}
+
+		// Show a dialog so that the user can also open the folder with the builds in it if they want
+		if (EditorUtility.DisplayDialog ("Builds complete!", "Your builds were created for Pollati's class!", "Show Build Folder", "Okay")) {
+			EditorUtility.RevealInFinder (baseDir);
+		}
+	}
+}
+#endif
--- README.md
+++ README.md
@@ -10,11 +10,16 @@ In the [Releases](https://github.com/ProfPollati/ProfPollatiUnityUtilities/relea
 This basic script will produce the Linux, Mac, and Windows builds and zip them for you. That way, you have the required releases, named properly for the GitHub Releases required for submitting in your project.
 
 Once it completes, it will pop open a requester where you can hit _Okay_ or _Show Build Folder_ to have the Build folder show up in Explorer on Windows or in the Finder for Mac.
-
+![](images/BuildComplete.png)
 #### Notes
 In order to easily produce a ZIP file with Unity 5.4 or lower, the library [Ionic.Zip.Unity](https://github.com/r2d2rigo/dotnetzip-for-unity) from https://github.com/r2d2rigo/dotnetzip-for-unity in needed in the Assets/Plugin/ folder.
+
 
 ### Launch Duplicator
 When trying to test out your project for the Multiplayer assignments, you either have to make a build of the game and launch that build and launch the editor, or you can make a duplicate of your project and launch that in another instance of Unity. Launching another instance makes it a little faster to changing things around, but it can take more time to copy the files, _unless_ you use this script.
 
-This will add a window with controls for you to Save, Duplicate and Launch. If you are running the game, it will not save the scenes or project since it may lead to issue with project.
+![](images/LaunchWindow.png)
+
+This will add a window with controls for you to **Save, Duplicate and Launch**. If you are running the game, it will only **Duplicate and Launch**. You cannot save the scenes or project since it may lead to issue with project. 
+
+Expanding the Details will tell you the location of the project, where it gets duplicated to, and the location of Unity's executable which you can edit incase your is in a different location.
