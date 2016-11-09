37ad715461d1630febbef6a514e29704cb6b4385
Christopher Pollati <christopher_pollati@bloomfield.edu>
2016-11-08 21:25:19 -0500

    No longer fails, but shows which builds are not available to be made

--- ProfPollatiUtilities/Assets/Scripts/Editor/BuildBuddy.cs
+++ ProfPollatiUtilities/Assets/Scripts/Editor/BuildBuddy.cs
@@ -1,5 +1,6 @@
 ﻿#if UNITY_EDITOR
 using UnityEditor;
+using System;
 using System.Diagnostics;
 using UnityEngine;
 using System.IO.Compression;
@@ -23,6 +24,8 @@ public class BuildBuddy : MonoBehaviour {
 		string buildPath;
 		string buildName;
 
+		string failures = "";
+
 		for(var i=0;i<targets.Length;i++) {
 			// Figure the name of the folder to build to
 			buildPath = baseDir + projectName + targetPrefix [i];
@@ -34,23 +37,34 @@ public class BuildBuddy : MonoBehaviour {
 				buildName = projectName + ".exe";
 			}
 
-			// Build
-			UnityEngine.Debug.Log ("Building"+targetPrefix[i]);
-			BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, buildPath + Path.DirectorySeparatorChar + buildName, targets[i], BuildOptions.None);
+			try {
+				// Build
+				UnityEngine.Debug.Log ("Building"+targetPrefix[i]);
+				BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, buildPath + Path.DirectorySeparatorChar + buildName, targets[i], BuildOptions.None);
 
-			// Zip
-			UnityEngine.Debug.Log ("Zipping"+targetPrefix[i]);
-			using (ZipFile zip = new ZipFile()) {
-				zip.AddDirectory (buildPath + Path.DirectorySeparatorChar);
-				zip.Save(buildPath + ".zip");
+				// Zip
+				UnityEngine.Debug.Log ("Zipping"+targetPrefix[i]);
+				using (ZipFile zip = new ZipFile()) {
+					zip.AddDirectory (buildPath + Path.DirectorySeparatorChar);
+					zip.Save(buildPath + ".zip");
+				}
+
+				// Done
+				UnityEngine.Debug.Log ("Building"+targetPrefix[i]+ " Complete!");
+			} catch(Exception e) {
+				// Failed...
+				UnityEngine.Debug.Log ("Building"+targetPrefix[i]+ " FAILED!");
+				failures += "\nFailed Building"+targetPrefix[i];
 			}
-
-			// Done
-			UnityEngine.Debug.Log ("Building"+targetPrefix[i]+ " Complete!");
 		}
 
 		// Show a dialog so that the user can also open the folder with the builds in it if they want
-		if (EditorUtility.DisplayDialog ("Builds complete!", "Your builds were created for Pollati's class!", "Show Build Folder", "Okay")) {
+		string buildCompleteMessage = "Your builds were created for Pollati's class!";
+		if(failures.Length>0) {
+			buildCompleteMessage += "\n\nHowever, the following targets were not built:\n" + failures;
+		}
+
+		if (EditorUtility.DisplayDialog ("Builds complete!", buildCompleteMessage, "Show Build Folder", "Okay")) {
 			EditorUtility.RevealInFinder (baseDir);
 		}
 	}
